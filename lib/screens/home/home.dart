4ea934c4c63385145709c75d67ef289d8e8ed197
Mathias <mvbend@stud.ntnu.no>
2021-10-25 15:12:26 +0200

    Bottom App Bar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:workout_app/services/auth.dart';
 
-class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+  }
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'HOME',
+      style: optionStyle,
+    ),
+    Text(
+      'RECORD',
+      style: optionStyle,
+    ),
+    Text(
+      'MY PAGE',
+      style: optionStyle,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.brown[50],
       appBar: AppBar(
-        title: const Text('My first flutter app'),
+        title: const Text('Fitness App'),
         backgroundColor: Colors.brown[400],
         elevation: 0.0, //no dropshadow / flat on the screen
         actions: <Widget>[
@@ -24,9 +48,31 @@ class Home extends StatelessWidget {
             label: const Text("Logout"),
             icon: const Icon(Icons.person),
           ),
+
         ],
+
       ),
-      body: Container(),
-    );
-  }
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fiber_manual_record_rounded),
+            label: 'New Workout/Record',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'My Page',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );  }
 }
