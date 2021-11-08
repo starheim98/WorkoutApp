30ebda9e9eee2fe68cb6b0aad5baf7e9c1c3964c
Andreas <andreas.hernas@gmail.com>
2021-11-08 09:30:30 +0100

    added friend list to accout data class

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -10,7 +10,17 @@ class Account{
 class AccountData{
   final String uid; //Unique ID of user.
   final String name; //User's name.
+  List<String>? friends;
 
   AccountData({required this.uid, required this.name}); //Constructor
+
+  bool addFriend(String uid){
+    bool success = false;
+    if(!friends!.contains(uid)){
+      friends!.add(uid);
+      success = true;
+    }
+    return success;
+  }
 
 }
