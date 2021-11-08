e3213f461a0b11e7ee7596ee1f4bc2ff8e3bdb2c
Andreas <andreas.hernas@gmail.com>
2021-11-08 10:06:50 +0100

    weightlifting template class added

--- lib/models/weight_lifting/exercise.dart
+++ lib/models/weight_lifting/exercise.dart
@@ -1,13 +1,17 @@
 
 class Exercise{
   final String name;
-  List<Set> sets;
+  List<Set>? sets;
 
-  Exercise(this.name, this.sets);
+  Exercise(this.name){
+    sets = [];
+  }
 
-  void addSet(Set set){}
+  void addSet(Set set){
+    sets!.add(set);
+  }
 
   void removeSet(Set set){
-    sets.remove(set);
+    sets!.remove(set);
   }
 }
