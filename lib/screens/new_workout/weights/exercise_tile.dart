c5d11a6bbe1654704f19f19aec4807120b6bb46b
Andreas <andreas.hernas@gmail.com>
2021-11-14 20:48:52 +0100

    weighlifting exercise in progress

--- lib/screens/new_workout/weights/exercise_tile.dart
+++ lib/screens/new_workout/weights/exercise_tile.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:workout_app/models/weight_lifting/exercise.dart';
-import 'package:workout_app/screens/new_workout/set_list.dart';
+import 'package:workout_app/models/weight_lifting/weight_workout.dart';
+import 'package:workout_app/screens/new_workout/weights/set_list.dart';
 
 class ExerciseTile extends StatefulWidget {
   Exercise exercise;
@@ -13,17 +14,28 @@ class ExerciseTile extends StatefulWidget {
 
 class _ExerciseTileState extends State<ExerciseTile> {
 
-
   @override
   Widget build(BuildContext context) {
     return Column(
         children: <Widget>[
-          Text(widget.exercise.getName()),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              widget.exercise.getName(),
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              )
+            ),
+          ),
           SetList(exercise: widget.exercise),
           ElevatedButton(
             onPressed: () => addSet(),
             child: Text("Add set"),
-
+          ),
+          const Divider(
+            color: Colors.black,
+            thickness: 2,
           )
         ]
     );
