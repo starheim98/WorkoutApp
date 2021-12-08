839dcfb27a2ca024c69ef1372d4ab9180551f968
Mathias <mvbend@stud.ntnu.no>
2021-12-08 21:08:28 +0100

    weight_details.dart skeleton complete.

--- lib/screens/home/my_page/workout_details/weight_details.dart
+++ lib/screens/home/my_page/workout_details/weight_details.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:workout_app/models/weight_lifting/exercise.dart';
 import 'package:workout_app/models/weight_lifting/weight_workout.dart';
 import 'package:workout_app/shared/constants.dart';
 
 class WorkoutDetailPage extends StatefulWidget {
   WeightWorkout workout;
+
   WorkoutDetailPage({Key? key, required this.workout}) : super(key: key);
 
   @override
@@ -13,11 +15,56 @@ class WorkoutDetailPage extends StatefulWidget {
 class _WorkoutDetailPageState extends State<WorkoutDetailPage> {
   @override
   Widget build(BuildContext context) {
+    SizedBox sizedBox =
+        SizedBox(height: 12, width: MediaQuery.of(context).size.width);
+    List<Exercise> exercises = widget.workout.exercises;
+    for (Exercise exercise in exercises) {
+      exercise.name;
+    }
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.workout.name!),
-      ),
-      body: Text("Penis :)"),
-    );
+        appBar: AppBar(
+          title: Text(widget.workout.name!),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Title",
+                  style: textStyle,
+                ),
+                Text(widget.workout.name!),
+                sizedBox,
+                const Text(
+                  "Exercises",
+                  style: textStyle,
+                ),
+                ListView.builder(
+                    padding: const EdgeInsets.all(12.0),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: exercises.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Center(child: Text(exercises[index].name)); // Ditta e for sentrere den
+                    }),
+                sizedBox,
+                const Text(
+                  "Duration",
+                  style: textStyle,
+                ),
+                Text(widget.workout.duration.toString()),
+                sizedBox,
+                const Text(
+                  "Date:",
+                  style: textStyle,
+                ),
+                Text(widget.workout.date.toString()),
+                sizedBox,
+              ],
+            ),
+          ),
+        ));
   }
 }
