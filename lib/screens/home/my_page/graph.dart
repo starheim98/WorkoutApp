5e628b2b9a169270189627f078966caae9f7adb7
Andreas <andreas.hernas@gmail.com>
2021-12-09 21:55:42 +0100

    progression page with graph completed

---
+++ lib/screens/home/my_page/graph.dart
@@ -0,0 +1,79 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+import 'package:workout_app/services/database.dart';
+
+class ProgressionGraph extends StatefulWidget {
+  String exercise;
+  bool isSelected;
+  ProgressionGraph({Key? key, required this.exercise, required this.isSelected})
+      : super(key: key);
+
+  @override
+  _ProgressionGraphState createState() => _ProgressionGraphState();
+}
+
+class _ProgressionGraphState extends State<ProgressionGraph> {
+  DatabaseService databaseService = DatabaseService();
+  List<DateAndWeight> data = [];
+  // List<charts.Series<DateAndWeight>> series = [];
+  bool animate = false;
+
+  @override
+  void initState() {
+    fetchExerciseData();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<charts.Series<DateAndWeight, DateTime>> series = [
+      charts.Series(
+          id: "exercise",
+          data: data,
+          domainFn: (DateAndWeight series, _) => series.date,
+          measureFn: (DateAndWeight series, _) => series.weight,
+      )
+    ];
+    if(data.length <= 1) {
+      return Column(children: const <Widget>[
+        SizedBox(height: 30),
+        Text("need more data ellerno")
+      ],);
+    } else {
+      return charts.TimeSeriesChart(series,
+        animate: true,
+        behaviors: [
+          charts.ChartTitle('Date',
+              behaviorPosition: charts.BehaviorPosition.bottom,
+              titleStyleSpec: const charts.TextStyleSpec(fontSize: 15),
+              titleOutsideJustification:
+              charts.OutsideJustification.middleDrawArea),
+          charts.ChartTitle('Weight, kg',
+              behaviorPosition: charts.BehaviorPosition.start,
+              titleStyleSpec: const charts.TextStyleSpec(fontSize: 15),
+              titleOutsideJustification:
+              charts.OutsideJustification.middleDrawArea)
+        ],
+      );
+    }
+  }
+
+  void fetchExerciseData() async {
+    var result = await databaseService.getExerciseData(widget.exercise);
+    result.sort((a, b) => a.date.compareTo(b.date));
+    setState(() {
+      data = result;
+    });
+  }
+}
+
+class DateAndWeight {
+  final DateTime date;
+  final int weight;
+
+  DateAndWeight(this.date, this.weight);
+}
