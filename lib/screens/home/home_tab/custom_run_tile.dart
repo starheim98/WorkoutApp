19e380885f2ccbb0ebbd6c9d5a6217cad05e5b09
Mathias <mvbend@stud.ntnu.no>
2021-12-10 21:00:20 +0100

    started om km/minute, and started styling

--- lib/screens/home/home_tab/custom_run_tile.dart
+++ lib/screens/home/home_tab/custom_run_tile.dart
@@ -4,6 +4,8 @@ import 'package:latlong2/latlong.dart';
 import 'package:workout_app/models/running/run_workout.dart';
 import 'package:workout_app/screens/home/my_page/workout_details/run_details.dart';
 import 'package:workout_app/services/database.dart';
+import 'package:workout_app/shared/constants.dart';
+import 'dart:math';
 
 import '../../../top_secret.dart';
 
@@ -40,28 +42,40 @@ class _CustomRunTileState extends State<CustomRunTile> {
                     RunWorkoutDetailsPage(runWorkout: runWorkout!)),
           )
         },
-        child: SizedBox(
+        child: Container(
           height: MediaQuery.of(context).size.height * 0.2,
           width: MediaQuery.of(context).size.width * 1,
+          padding: EdgeInsets.all(6),
           child: Row(
             children: [
-              Column(
-                children: [
-                  Text(name, style: TextStyle(fontSize: 12), overflow: TextOverflow.ellipsis,),
-                  Text("Date: " + runWorkout!.date, style: TextStyle(fontSize: 10)),
-                  Text("\nTitle: " + runWorkout!.title, style: TextStyle(),),
-                  Text("\nDescription: " + runWorkout!.description, style: TextStyle(), overflow: TextOverflow.ellipsis,),
-                  Text("Time spent: " + runWorkout!.duration, style: TextStyle()),
-                  Text("Distance: " + runWorkout!.distance, style: TextStyle()),
-                ],
+              Flexible(
+                child: Column(
+                  children: [
+                    Text(name, style: tileName),
+                    Text(runWorkout!.title, style: tileTitle),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(runWorkout!.duration, style: TextStyle()),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(runWorkout!.distance, style: TextStyle()),
+                      ],
+                    ),
+                    Text(timePerKm(runWorkout!).toString()),
+                    Text("\n\n"),
+                    Text(runWorkout!.date, style: tileDate),
+                  ],
+                ),
               ),
               SizedBox(width: 10),
               Container(
                 width: MediaQuery.of(context).size.width * 0.43,
-                padding: EdgeInsets.all(2),
                 child: FlutterMap(
                   options: MapOptions(
-                    center: LatLng(runWorkout!.getPoints().last.latitude, runWorkout!.getPoints().last.longitude),
+                    center: LatLng(runWorkout!.getPoints().last.latitude,
+                        runWorkout!.getPoints().last.longitude),
                     zoom: 15.0,
                   ),
                   layers: [
@@ -84,9 +98,39 @@ class _CustomRunTileState extends State<CustomRunTile> {
     );
   }
 
+  Duration parseDuration(String s) {
+    int hours = 0;
+    int minutes = 0;
+    int micros;
+    List<String> parts = s.split(':');
+    if (parts.length > 2) {
+      hours = int.parse(parts[parts.length - 3]);
+    }
+    if (parts.length > 1) {
+      minutes = int.parse(parts[parts.length - 2]);
+    }
+    micros = (double.parse(parts[parts.length - 1]) * 1000000).round();
+    return Duration(hours: hours, minutes: minutes, microseconds: micros);
+  }
+
+  //minutt + sekund, per kilometer
+  //05:20 per
+  // SECONDS PER KM
+  double timePerKm(RunWorkout runWorkout){
+    Duration duration = parseDuration(runWorkout.duration);
+
+    int persecond = duration.inSeconds;
+
+    double kms = 0.0;
+    if (persecond!=0) {
+      kms = persecond/double.parse(runWorkout.distance);
+    }
+    return kms;
+  }
+
   Future getName(String uid) async {
     var result = await databaseService.getUser(uid);
-    if (mounted){
+    if (mounted) {
       setState(() {
         name = result.getName();
       });
--- lib/screens/home/home_tab/custom_weightworkout_tile.dart
+++ lib/screens/home/home_tab/custom_weightworkout_tile.dart
@@ -3,6 +3,7 @@ import 'package:workout_app/models/weight_lifting/exercise.dart';
 import 'package:workout_app/models/weight_lifting/weight_workout.dart';
 import 'package:workout_app/screens/home/my_page/workout_details/weight_details.dart';
 import 'package:workout_app/services/database.dart';
+import 'package:workout_app/shared/constants.dart';
 
 class CustomWeightworkoutTile extends StatefulWidget {
   WeightWorkout weightWorkout;
@@ -49,25 +50,30 @@ class _CustomWeightworkoutTileState extends State<CustomWeightworkoutTile> {
           )
         },
         child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.2,
           width: MediaQuery.of(context).size.width * 1,
           child: Row(
             children: [
               Container(
-                width: MediaQuery.of(context).size.width * 0.1,
                 height: MediaQuery.of(context).size.width * 0.2,
                 padding: EdgeInsets.all(5),
-                child: Icon(Icons.train),
+                child: Icon(Icons.train), // RANDOM ICON TODO: Find something better
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.4,
-                child: Column(
-                  children: [
-                    Text("name: " + name),
-                    Text("Workout name: " + weightWorkout!.name!),
-                    Text("Time spent: " + weightWorkout!.duration.toString()),
-                  ],
+
+              Flexible( //Flexible for overflowing text
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Column(
+                    children: [
+                      Text(name, style: tileName),
+                      Text("\nWorkout name: " + weightWorkout!.name!, style: tileTitle),
+                      Text(weightWorkout!.date!, style: tileDate),
+                      Text("Time spent: " + weightWorkout!.duration.toString(), style: const TextStyle(overflow: TextOverflow.ellipsis)),
+                    ],
+                  ),
                 ),
               ),
+
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.3,
                 height: MediaQuery.of(context).size.width * 0.4,
