60cecff3969875d8327c53dbd57a645189b777cf
Andreas <andreas.hernas@gmail.com>
2021-12-15 17:46:11 +0100

    create new template styling

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -9,7 +9,7 @@ const textInputDecoration = InputDecoration(
         borderSide: BorderSide(color: Colors.white, width: 2.0)
     ),
     focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.pink, width: 2.0)
+        borderSide: BorderSide(color: Colors.blue, width: 2.0)
     )
 );
 
