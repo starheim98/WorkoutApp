acbf51bad4486ebc82285063af4b61e17ab08800
Andreas <andreas.hernas@gmail.com>
2021-12-15 16:35:24 +0100

    select template or new workout styling

--- lib/screens/new_workout/choose_new_or_template.dart
+++ lib/screens/new_workout/choose_new_or_template.dart
@@ -1,48 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:workout_app/screens/new_workout/weights/workout_templates/template_page.dart';
 import 'package:workout_app/screens/new_workout/weights/weightlift_screen.dart';
+import 'package:workout_app/shared/constants.dart';
 
 class WeightLifting extends StatefulWidget {
   const WeightLifting({Key? key}) : super(key: key);
-
 
   @override
   State<WeightLifting> createState() => _WeightLiftingState();
 }
 
 class _WeightLiftingState extends State<WeightLifting> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
         appBar: AppBar(
           title: const Text("Weightlifting"),
         ),
         body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const TemplatePage()),
-                    )
-                  },
-                  child: const Text('Select from template'),
+          child: Column(children: <Widget>[
+            const SizedBox(height: 44,),
+            GestureDetector(
+              onTap: () async => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TemplatePage()),
+                )
+              },
+              child: SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: Card(
+                  elevation: 2,
+                    margin: EdgeInsets.only(left: 16, right: 16),
+                    child: Center(
+                      child: GradientText(
+                        "Select from template",
+                        style: const TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold
+                        ),
+                        colors: const [Color(0xff4574EB), Color(0xff005FB7)],
+                      ),
+                    ),
                 ),
-                ElevatedButton(
-                  onPressed: () async => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => NewWorkout()),
-                    )
-                  },
-                  child: const Text('New workout'),
-                ),
-              ]),
+              ),
+            ),
+            const SizedBox(height: 18),
+            GestureDetector(
+              onTap: () async => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NewWorkout()),
+                )
+              },
+              child: SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: Card(
+                  elevation: 2,
+                    margin: const EdgeInsets.only(left: 16, right: 16),
+                    child: Center(
+                      child: GradientText(
+                        "New workout",
+                        style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                        ),
+                        colors: const [Color(0xff4574EB), Color(0xff005FB7)],
+                      ),
+                    )),
+              ),
+            ),
+          ]),
         ));
   }
 }
