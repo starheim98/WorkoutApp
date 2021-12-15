3c3da5c904f806cf15cb1b0aa725f6836899ae1a
Andreas <andreas.hernas@gmail.com>
2021-12-15 13:09:53 +0100

    select training styling done

--- lib/screens/home/new_workout_tab/new_workout_screen.dart
+++ lib/screens/home/new_workout_tab/new_workout_screen.dart
@@ -1,33 +1,99 @@
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:workout_app/screens/new_workout/choose_new_or_template.dart';
 import 'package:workout_app/screens/new_workout/running/running_screen.dart';
 
 Column newWorkoutTab(BuildContext context) =>
     Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        const Text("Select form of training:"),
-        const SizedBox(height: 50.0),
-        ElevatedButton(
-            onPressed: () async => {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Running()),
-              )
-            },
-            child:
-            const Text("Running", style: TextStyle(color: Colors.white))),
-        ElevatedButton(
-            onPressed: () async => {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const WeightLifting()),
-              )
-            },
-            child: const Text("Weightlifting",
-                style: TextStyle(color: Colors.white))),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Text("Select training", style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),),
+        ),
+        GestureDetector(
+          onTap: () async => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const WeightLifting()),
+            )
+          },
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 16, right: 16),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(3),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                )
+              ]
+
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(child: Image.asset('lib/assets/weight.png', height: 80,), flex: 4),
+                const Expanded(
+                  child: Text("Weightlifting", style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff005FB7)
+                  ),),
+                  flex: 6,
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 18),
+        GestureDetector(
+          onTap: () async => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Running()),
+            )
+          },
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 16, right: 16),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(3),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  )
+                ]
+
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(child: Image.asset('lib/assets/run.png'), flex: 4,),
+                // Expanded(child: Icon(Icons.fitness_center), flex: 4,),
+                const Expanded(
+                  child: Text("Running", style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xffC9082B)
+                  ),),
+                  flex: 6,
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
