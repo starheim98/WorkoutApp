e60b7621a8674e5a40156c85d8f7765c5ffc8afc
Andreas <andreas.hernas@gmail.com>
2021-12-15 18:31:35 +0100

    template page in progress fixed bug

--- lib/screens/new_workout/weights/workout_templates/template_page.dart
+++ lib/screens/new_workout/weights/workout_templates/template_page.dart
@@ -32,7 +32,7 @@ class _TemplatePageState extends State<TemplatePage> {
         child: Column(
           children: <Widget>[
             SizedBox(height: 20),
-            const Text("Your templates", style: TextStyle(fontSize: 25),),
+            const Text("Your templates", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
             SizedBox(height: 20),
             ListView.builder(
               scrollDirection: Axis.vertical,
@@ -41,23 +41,45 @@ class _TemplatePageState extends State<TemplatePage> {
               itemBuilder: (BuildContext context, int index) {
                 WorkoutTemplate template = templates[index];
                 return Card(
-                  child: ListTile(
-                    subtitle: ConstrainedBox(
-                      constraints: const BoxConstraints(maxHeight: 60, minHeight: 60),
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        itemCount: template.exercises.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Text(template.exercises[index]);
-                        },
+                  // child: ListTile(
+                  //   subtitle: ConstrainedBox(
+                  //     constraints: const BoxConstraints(maxHeight: 60, minHeight: 60),
+                  //     child: ListView.builder(
+                  //       shrinkWrap: true,
+                  //       scrollDirection: Axis.vertical,
+                  //       itemCount: template.exercises.length,
+                  //       itemBuilder: (BuildContext context, int index) {
+                  //         return Text(template.exercises[index]);
+                  //       },
+                  //     ),
+                  //   ),
+                  //   onTap: () => selectTemplate(template, context),
+                  //   title: Text(template.name),
+                  //   trailing: IconButton(
+                  //     onPressed: () => deleteTemplate(template.id!), icon: const Icon(Icons.delete),
+                  //   ),
+                  // ),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: Image.asset('lib/assets/weight.png'), flex: 2),
+                      Expanded(
+                        flex: 5,
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                  template.name
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    onTap: () => selectTemplate(template, context),
-                    title: Text(template.name),
-                    trailing: IconButton(
-                      onPressed: () => deleteTemplate(template.id!), icon: const Icon(Icons.delete),
-                    ),
+                      const Expanded(
+                        flex: 2,
+                        child: Icon(Icons.delete),
+                      ),
+                    ],
                   ),
                 );
               },
